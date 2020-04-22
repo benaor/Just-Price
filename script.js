f20d1d98b3742e36ef587fc2396e474392d60d63
benaor <benjamingirard25@gmail.com>
2020-04-22 21:52:14 +0200

    select elements

---
+++ script.js
@@ -0,0 +1,5 @@
+// Etape 1 - Sélectionner nos éléments
+let proposition = document.getElementById('prix');
+let btnValidate = document.getElementsByTagName('button');
+let erreur      = document.getElementById('erreur');
+
