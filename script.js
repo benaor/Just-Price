a79b725df09e2a3647e5a60e14a5dec33e982c40
benaor <benjamingirard25@gmail.com>
2020-04-22 21:55:48 +0200

    generate a ramdom number

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ let erreur      = document.getElementById('erreur');
 
 // Etape 2 - Cacher l'erreur
 erreur.style = 'display : none';
+
+// Etape 3 - Générer un nombre aléatoire
+let aleatoire = Math.floor( Math.random() * 1000);
+
+
+// Etape 4 - Vérifier que l'utilisateur donne bien un nombre
+
+// Etape 5 - Agir à l'envoi du formulaire
+
+// Etape 6 - Créer la fonction vérifier
