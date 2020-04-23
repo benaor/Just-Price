fc0eccce9e3290c6336ca5fe98c435928fd6b814
benaor <benjamingirard25@gmail.com>
2020-04-23 23:09:55 +0200

    Verif if proposition is a number

--- script.js
+++ script.js
@@ -1,34 +1,54 @@
 // Etape 1 - Sélectionner nos éléments
-let proposition = document.getElementById('prix');
+let input = document.getElementById('prix');
 let btnValidate = document.getElementById('btnValidate');
-let erreur      = document.getElementById('erreur');
+let erreur = document.getElementById('erreur');
 let instruction = document.getElementById('instructions');
+let formulaire = document.getElementById('formulaire');
 
 // Etape 2 - Cacher l'erreur et désactiver le bouton
 erreur.style = 'display : none';
-btnValidate.disabled = true;
+// btnValidate.disabled = true;
 
-// Etape 3 - Générer un nombre aléatoire
-let aleatoire = Math.floor( Math.random() * 1001);
+// Etape 3 - Générer un nombre aléatoire et la variable coups
+let aleatoire = Math.floor(Math.random() * 1001);
+let coups = 0;
+let nombreChoisi;
+console.log(aleatoire);
 
 // Etape 4 - Vérifier que l'utilisateur donne bien un nombre
-proposition.addEventListener('keyup', () => {
+input.addEventListener('keyup', () => {
 
-    if(isNaN(proposition.value) || proposition.value==""){
+    if (isNaN(input.value) || input.value == "") {
 
         erreur.style = 'display : inline';
-        btnValidate.disabled = true;
+        // btnValidate.disabled = true;
 
     } else {
 
         erreur.style = 'display : none';
-        btnValidate.disabled = false;
+        // btnValidate.disabled = false;
 
     }
 })
 
 // Etape 5 - Agir à l'envoi du formulaire
-// btnValidate.addEventListener('click', () => {
+formulaire.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-// })
-// Etape 6 - Créer la fonction vérifier
+    if (isNaN(input.value) || input.value == "") {
+
+        input.style = 'border-color : red';
+        // btnValidate.disabled = true;
+
+    } else {
+
+        coups++;
+        input.style = 'border-color : silver';
+        nombreChoisi = input.value;
+        input.value = '';
+        verifier(nombreChoisi);
+        // btnValidate.disabled = false;
+
+    }
+})
+
